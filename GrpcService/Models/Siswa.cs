2fe74bb386a950176c72157c0a42992a57a07527
sigit.darmawan <sigit.darmawan@bankraya.co.id>
2022-04-30 18:07:02 +0700

    Add project files.

---
+++ GrpcService/Models/Siswa.cs
@@ -0,0 +1,10 @@
+﻿namespace GrpcService.Models
+{
+    public class Siswa
+    {
+        public int SiswaId { get; set; }
+        public string NamaDepan { get; set; } = string.Empty;
+        public string NamaBel { get; set; } = string.Empty;
+        public string Sekolah { get; set; } = string.Empty;
+    }
+}
