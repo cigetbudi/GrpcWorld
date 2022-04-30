38b64377c14386a95c0d48c89c2299af279b3e36
sigit.darmawan <sigit.darmawan@bankraya.co.id>
2022-04-30 18:27:55 +0700

    Fixing typo in appsetting.json and do a add-migration and update-database

---
+++ GrpcService/Data/Migrations/20220430112713_M1.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GrpcService.Data.Migrations
+{
+    public partial class M1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Siswas",
+                columns: table => new
+                {
+                    SiswaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NamaDepan = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NamaBel = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Sekolah = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Siswas", x => x.SiswaId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Siswas",
+                columns: new[] { "SiswaId", "NamaBel", "NamaDepan", "Sekolah" },
+                values: new object[,]
+                {
+                    { 11, "Fox", "Ann", "Nursing" },
+                    { 22, "Doe", "Sam", "Mining" },
+                    { 33, "Cox", "Sue", "Business" },
+                    { 44, "Lee", "Tim", "Computing" },
+                    { 55, "Ray", "Jan", "Computing" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Siswas");
+        }
+    }
+}
